82600336c75ca70017d6da0233cf5620fd7c0fc6
Deepali Chourasia <deepch23@in.ibm.com>
2020-11-23 15:03:28 +0000

    upgrade to 2.5.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,10 +53,16 @@ cp make.inc-examples/make.inc.openblas ./make.inc
 # standard supported by both tool sets.
 export CXXFLAGS="$(echo $CXXFLAGS | sed -e 's/ -std=[^ ]*//') -std=c++14"
 
+GPU_TARGET="sm_37 sm_60 sm_70 sm_75"
+if [[ $cudatoolkit == '11.0' ]]
+then
+  GPU_TARGET="$GPU_TARGET sm_80"
+fi
+
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${CUDA_HOME}/include -I${CONDA_PREFIX}/include"
 mkdir build
 cd build
-cmake .. -DUSE_FORTRAN=OFF -DGPU_TARGET="sm_37 sm_60 sm_70 sm_75" -DCMAKE_INSTALL_PREFIX=$PREFIX \
-         -DCMAKE_CXX_FLAGS=" -I${PREFIX}/include -I${CUDA_HOME}/include -I/usr/include" 
+cmake .. -DUSE_FORTRAN=OFF -DGPU_TARGET="$GPU_TARGET"  -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j$(getconf _NPROCESSORS_CONF)
 make install
 cd ..
